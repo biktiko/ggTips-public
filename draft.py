8b37af0cb086d1ada71e58ab22acc07d6eda9ef3
biktiko <btigran02@gmail.com>
2024-08-24 22:55:13 +0400

    More settings and customize for users, UI upgrade, graph upgrade, started work with companies data

--- draft.py
+++ draft.py
@@ -38,7 +38,6 @@ def replace_values(df):
     return df
 
 def load_data():
-    
     if not os.path.exists(uploadFilesPath):
         os.makedirs(uploadFilesPath)
 
@@ -84,42 +83,34 @@ def load_data():
 
             for sheet in newExcelFile.sheet_names:
                 df_header = pd.read_excel(newExcelFile, sheet_name=sheet, nrows=0)
+                df_header.columns = [col.strip().lower() for col in df_header.columns]  # Приведение всех заголовков к нижнему регистру и убирание пробелов
 
-                # Проверка наличия хотя бы одного ключевого слова в заголовках столбцов для Tips
-                for table in tablesKeyWords.keys():
-                    if any(keyword in df_header.columns for keyword in tablesKeyWords[table]):
-                        if table=="Tips":
-                            columns_to_load = []
-                            for column in df_header.columns:
-                                if isinstance(column, str):
-                                    column_stripped = column.strip().lower()
-                                    for key, keywords in columnsKeyWords.items():
-                                        if column_stripped in keywords:
-                                            columns_to_load.append(column)
+                # Проверка наличия хотя бы одного ключевого слова в заголовках столбцов для Tips и других таблиц
+                for table, keywords in tablesKeyWords.items():
+                    if any(keyword.lower() in df_header.columns for keyword in keywords):
+                        columns_to_load = [col for col in df_header.columns if any(col in keywords for keyword in columnsKeyWords.values())]
+                        if columns_to_load:
+                            df = pd.read_excel(newExcelFile, sheet_name=sheet, usecols=columns_to_load, engine='openpyxl')
+                            df.columns = df.columns.str.strip().str.lower()  # Приведение всех заголовков к нижнему регистру
 
-                            if columns_to_load:
-                                df = pd.read_excel(newExcelFile, sheet_name=sheet, usecols=columns_to_load, engine='openpyxl')
-                                df = df.copy()
-
+                            if table == 'Tips':
                                 # Переименование столбцов
                                 renamed_columns = {}
                                 for column in df.columns:
-                                    column_stripped = column.strip().lower()
                                     for key, keywords in columnsKeyWords.items():
-                                        if column_stripped in keywords:
+                                        if column in keywords:
                                             renamed_columns[column] = key
 
                                 df.rename(columns=renamed_columns, inplace=True)
                                 df = replace_values(df)
 
                                 # Обработка времени
-                                if 'Date' in df.columns:
-                                    df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
+                                if 'date' in df.columns:
+                                    df['date'] = pd.to_datetime(df['date'], errors='coerce')
 
                                 # Проверка и объединение данных по uuid
                                 if 'uuid' in df.columns:
                                     df.set_index('uuid', inplace=True)
-                                    
                                     df = df[~df.index.duplicated(keep='last')]
 
                                     if not Tips.empty:
@@ -132,20 +123,21 @@ def load_data():
                                         Tips = pd.concat([Tips, df.reset_index()], ignore_index=True)
                                 else:
                                     print(f"'uuid' column not found in the sheet {sheet}")
-                        elif table=='Companies':
-                                print("Company sheet is ", sheet)
+                            
+                            elif table == 'Companies':
                                 df_companies = pd.read_excel(newExcelFile, sheet_name=sheet, engine='openpyxl')
-                                companies = pd.concat([companies, df_companies])
-
+                                df_companies.columns = df_companies.columns.str.strip().str.lower()  # Приведение всех заголовков к нижнему регистру
+                                companies = pd.concat([companies, df_companies], ignore_index=True)
+                    
                     # Обработка листа с данными о ggTeammates
                     elif sheet.lower() == 'gg teammates':  # Проверка имени листа без учета регистра
                         df_teammates = pd.read_excel(newExcelFile, sheet_name=sheet, engine='openpyxl')
 
-                        if 'ID' in df_teammates.columns and 'NUMBER' in df_teammates.columns:
-                            ggTeammates['id'] = df_teammates['ID']
-                            ggTeammates['number'] = df_teammates['NUMBER']
+                        if 'id' in df_teammates.columns and 'number' in df_teammates.columns:
+                            ggTeammates['id'] = df_teammates['id']
+                            ggTeammates['number'] = df_teammates['number']
                         else:
-                            print(f"'ID' or 'NUMBER' column not found in the sheet {sheet}")
+                            print(f"'id' or 'number' column not found in the sheet {sheet}")
 
     # Обработка временных меток и создание дополнительных столбцов
     if 'Date' in Tips.columns:
--- ggTipsProject.py
+++ ggTipsProject.py
@@ -1,8 +1,7 @@
 import os
 import pandas as pd
 import streamlit as st
-import plotly.express as px
-from streamlit_plotly_events import plotly_events
+import altair as alt
 from data.ggTipsData import load_data, uploadFilesPath
 from ggtipsconfig import login
 
@@ -14,10 +13,13 @@ if st.session_state['authentication_status']:
   
     data = load_data()
     
+    companies = data['companies']
     tips = data['tips']
     defaultInputs = data['defaultInputs']
     ggTeammates = data['ggTeammates']
-        
+    
+    filteredTips = tips.copy()
+
     for setting in defaultInputs.keys():
         if setting not in st.session_state:
             st.session_state[setting] = defaultInputs[setting]
@@ -36,57 +38,10 @@ if st.session_state['authentication_status']:
             st.success('New data added')
             st.session_state['uploadedFiles'].append(newFilePath)
 
-        uploadedFiles = st.session_state['uploadedFiles']
-
-        col1, col2 = st.columns(2)
-
-        importedFilesDetails = st.checkbox("Imported Files details")
-
-        if importedFilesDetails and uploadedFiles:
-            for file in uploadedFiles:
-                st.write(f"File: {os.path.basename(file)}")
-                st.write(f"Size: {os.path.getsize(file) / 1024:.2f} KB")
-                if file.endswith('.xlsx'):
-                    df = pd.read_excel(file)
-                else:
-                    df = pd.read_csv(file)
-                st.write(f"Number of rows: {len(df)}")
-                st.write(f"Columns: {', '.join(df.columns)}")
-                st.write("---")
-
-        clearFolderButtonClicked = st.button("Delete all new files", key="clearFolderButton")
-
-        if clearFolderButtonClicked:
-                for file in os.listdir(uploadFilesPath):
-                    file_path = os.path.join(uploadFilesPath, file)
-                    if os.path.isfile(file_path):
-                        try:
-                            os.remove(file_path)
-                            st.session_state['uploadedFiles'] = []
-                            st.success("Imported data cleared")
-                        except PermissionError as e:
-                            st.error(f"Failed to delete {file_path}: {e}")
-                        except Exception as e:
-                            st.error(f"Unexpected error when deleting {file_path}: {e}")
-
     st.title('ggTips')
+    
 
     if 'tips' in data:
-        months = {
-            'January': 1,
-            'February': 2,
-            'March': 3,
-            'April': 4,
-            'May': 5,
-            'June': 6,
-            'July': 7,
-            'August': 8,
-            'September': 9,
-            'October': 10,
-            'November': 11,
-            'December': 12,
-        }
-        
         options = {
             'companiesOptions' : list(tips['Company'].unique()) if 'Company' in tips else ['All'],
             'partnersOptions' : list(tips['Partner'].unique()) if 'Partner' in tips else ['All'],
@@ -95,6 +50,62 @@ if st.session_state['authentication_status']:
             'ggPayeersOptions' : ['All', 'Wihout gg teammates', 'Only gg teammates']
         }
 
+        # Настройки боковой панели
+        with st.sidebar:
+            st.header('Settings')
+            
+            with st.expander('ggTips filters'):
+                        
+                date_range = st.date_input("Select date range", [])
+                
+                if date_range:
+                    start_date, end_date = date_range
+                    filteredTips = filteredTips[(filteredTips['Date'] >= pd.to_datetime(start_date)) & (filteredTips['Date'] <= pd.to_datetime(end_date))]
+
+                col1, col2 = st.columns(2)
+
+                with col1:
+                    st.selectbox('ggPayers', options['ggPayeersOptions'], key='ggPayeers')
+
+                with col2:
+                    st.multiselect('Payment Processor', options['paymentProcessorOptions'], default=st.session_state['paymentProcessor'], key='paymentProcessor')
+
+                col1, col2 = st.columns(2)
+
+                with col1:
+                    st.number_input('Min amount', value=st.session_state['amountFilterMin'], step=1000, min_value=0, max_value=50000, key='amountFilterMin')
+
+                with col2:
+                    st.number_input('Max amount', value=st.session_state['amountFilterMax'], step=1000, min_value=st.session_state['amountFilterMin'], max_value=50000, key='amountFilterMax')
+                        
+            with st.expander('**Graph values**'):
+                sum_type = st.selectbox('Sum Chart Type', ['Column', 'Line', 'Area', 'None'])
+                count_type = st.selectbox('Count Chart Type', ['Line', 'Column', 'Area', 'None'])
+                
+            with st.expander('**Graph Customize**'):
+                
+                col1, col2 = st.columns(2)
+                with col1:
+                    sum_color = st.color_picker('Sum Color', '#00FF00')
+                with col2:
+                    count_color = st.color_picker('Count Color', '#0000FF')
+                    
+                text_color = st.color_picker('Text Color', '#FFFFFF')
+                chart_width = st.slider('Chart Width', 600, 1200, 1000)
+                chart_height = st.slider('Chart Height', 400, 800, 500)
+                reset_button = st.button('Reset Settings')
+
+            # Если нажата кнопка сброса
+            if reset_button:
+                st.session_state['chart_type'] = 'Column'
+                st.session_state['show_table'] = False
+                st.session_state['sum_color'] = '#00FF00'
+                st.session_state['count_color'] = '#0000FF'
+                st.session_state['text_color'] = '#FFFFFF'
+                st.session_state['chart_width'] = 1000
+                st.session_state['chart_height'] = 500
+                st.rerun()  # Перезапуск приложения для применения сброса настроек
+
         col1, col2 = st.columns(2)
 
         with col1:
@@ -102,169 +113,116 @@ if st.session_state['authentication_status']:
 
         with col2:
             st.multiselect('Select partners', options['partnersOptions'], key='selectedPartners')
-
-
-        with st.expander('More filters'):
-            
-            st.multiselect('Select month', list(months.keys()), key='selectedMonth')
-
-            timeIntervalOptions = ['All', 'Week', 'Month', 'Year', 'Week day', 'Day', 'Hour', 'Custom day']
-
-            if st.session_state['timeInterval'] != 'Custom day':
-                st.selectbox('Time interval', timeIntervalOptions, key='timeInterval')
-            else:
-
-                col1, col2 = st.columns(2)
-
-                with col1:
-                    st.selectbox('Time interval', timeIntervalOptions, index=timeIntervalOptions.index("Custom day"), key='timeInterval')
-
-                with col2:
-                    st.number_input('Custom', value=10, step=1, min_value=1)  # set max value
-                    
-            col1, col2, col3 = st.columns(3)
-
-            with col1:
-                st.selectbox('ggPayers', options['ggPayeersOptions'], key='ggPayeers')
-
-            with col2:
-                st.multiselect('Payment Processor', options['paymentProcessorOptions'], default=st.session_state['paymentProcessor'], key='paymentProcessor')
-
-            with col3:
-                st.multiselect('Payment Status', options['StatusOptions'], default=st.session_state['Status'], key='Status')
-
-            col1, col2 = st.columns(2)
-
-            with col1:
-                st.number_input('Min amount', value=st.session_state['amountFilterMin'], step=1000, min_value=0, max_value=50000, key='amountFilterMin')
-
-            with col2:
-                st.number_input('Max amount', value=st.session_state['amountFilterMax'], step=1000, min_value=st.session_state['amountFilterMin'], max_value=50000, key='amountFilterMax')
-
-
-        def setFilters():
-            filteredTips = tips.copy()
-
-            if 'selectedCompanies' in st.session_state and st.session_state['selectedCompanies']:
-                filteredTips = filteredTips[filteredTips['Company'].isin(st.session_state['selectedCompanies'])]
-            if 'selectedPartners' in st.session_state and st.session_state['selectedPartners']:
-                filteredTips = filteredTips[filteredTips['Partner'].isin(st.session_state['selectedPartners'])]
-            if 'selectedMonth' in st.session_state and st.session_state['selectedMonth']:
-                month_indices = [months[month] for month in st.session_state['selectedMonth']]
-                filteredTips = filteredTips[filteredTips['Month'].isin(month_indices)]
-            if 'Status' in st.session_state and st.session_state['Status']:
-                filteredTips = filteredTips[filteredTips['Status'].isin(st.session_state['Status'])]
-            if 'paymentProcessor' in st.session_state and st.session_state['paymentProcessor']:
-                filteredTips = filteredTips[filteredTips['Payment Processor'].isin(st.session_state['paymentProcessor'])]
-            if  'Amount' in filteredTips and 'amountFilterMin' in st.session_state and 'amountFilterMax' in st.session_state:
-                filteredTips = filteredTips[
-                    (filteredTips['Amount'] >= st.session_state['amountFilterMin']) &
-                    (filteredTips['Amount'] <= st.session_state['amountFilterMax'])
-                ]
-            
-            return filteredTips
         
-        filteredTips = setFilters()        
+        if 'selectedCompanies' in st.session_state and st.session_state['selectedCompanies']:
+            filteredTips = filteredTips[filteredTips['Company'].isin(st.session_state['selectedCompanies'])]
+        if 'selectedPartners' in st.session_state and st.session_state['selectedPartners']:
+            filteredTips = filteredTips[filteredTips['Partner'].isin(st.session_state['selectedPartners'])]
+        if 'Status' in st.session_state and st.session_state['Status']:
+            filteredTips = filteredTips[filteredTips['Status'].isin(st.session_state['Status'])]
+        if 'paymentProcessor' in st.session_state and st.session_state['paymentProcessor']:
+            filteredTips = filteredTips[filteredTips['Payment processor'].isin(st.session_state['paymentProcessor'])]
+        if  'Amount' in filteredTips and 'amountFilterMin' in st.session_state and 'amountFilterMax' in st.session_state:
+            filteredTips = filteredTips[
+                (filteredTips['Amount'] >= st.session_state['amountFilterMin']) &
+                (filteredTips['Amount'] <= st.session_state['amountFilterMax'])
+            ]
+        if st.session_state['ggPayeers'] and 'ggPayer' in filteredTips.columns:
+            if st.session_state['ggPayeers'] == 'Wihout gg teammates':
+                filteredTips = filteredTips[~filteredTips['ggPayer'].isin(ggTeammates['id'])]
+            elif st.session_state['ggPayeers'] == 'Only gg teammates':
+                filteredTips = filteredTips[filteredTips['ggPayer'].isin(ggTeammates['id'])]   
+                
+        groupedTips = filteredTips.groupby('Week').agg({
+            'Amount': 'sum',
+            'uuid': 'count',  
+            'WeekStart' : 'max',
+            'WeekEnd': 'max'
+        }).reset_index().rename(columns={'uuid': 'Count', 'Amount': 'ggTips'})
 
-        def tipsGroupBy(tips, timeInterval):
-            groupedTips = tips.groupby(timeInterval).agg({
-                'Amount': 'sum',
-                'uuid': 'count'  # Подсчет количества уникальных записей в каждой группе
-            }).reset_index().rename(columns={'uuid': 'Count', 'Amount': 'ggTips'})  # Переименуем колонку в 'Count'
+        groupedTips['Period'] = groupedTips['WeekStart'].dt.strftime('%Y-%m-%d') + ' - ' + groupedTips['WeekEnd'].dt.strftime('%Y-%m-%d')
 
-            return groupedTips
-        
-        if st.session_state['timeInterval'] and st.session_state['timeInterval'] != 'All':
-            filteredTips = tipsGroupBy(filteredTips, st.session_state['timeInterval'])
-    
-        if st.button('Clear Filters'):
-            for setting in defaultInputs.keys():
-                st.session_state[setting] = defaultInputs[setting]
-            st.experimental_rerun()
-
-        st.header('Chart')
-        
         with st.expander('stats'):
             col1, col2, col3 = st.columns(3)
+            sumTips = groupedTips['ggTips'].sum()
+            countTips = groupedTips['Count'].sum()
+            averageTip = sumTips / countTips
             
             with col1:
-                st.write('Sum: ', filteredTips['ggTips'].sum())
+                st.write('Sum: ', sumTips)
 
             with col2:
-                st.write('Count: ', filteredTips['Count'].sum())
+                st.write('Count: ', countTips)
+                
+            with col3:
+                st.write('One average tip', averageTip)
+                
+        # Создание графика с помощью Altair
+        tab1, tab2 = st.tabs(['Graph', 'Tables'])
+        
+        with tab1: 
             
-        st.selectbox('Aggretation', ['sum', 'count'], key='aggretation')
-        
-        if st.session_state['aggretation']=='sum':
-            y_axis = 'ggTips'
-        elif st.session_state['aggretation'] == 'count':
-            y_axis = 'Count'
-        
-        graph = px.bar(filteredTips, x=filteredTips.columns[0], y=y_axis,
-                                title=st.session_state['aggretation'],
-                                color_discrete_sequence=['green'],
-                        )
-        
-        graph.update_traces(
-            texttemplate='%{y:․0f}',
-            textposition='outside',
-            # hovertemplate='''<b>Week:  </b> %{Hour}<br>
-            #                  <b>Count: </b> <extra></extra><br><br>
-            #                  <b>Amount:</b> %{y}<extra></extra> ''',
-            hoverlabel=dict(
-                    bgcolor='#1f77b4',  # Цвет фона подсказки
-                    font_size=15,
-                    font_color='white'
-                ),
-            # customdata=filteredTips[['week_end', 'Tips Count']].values
-        )
+            if sum_type!='None' or count_type!='None':
+                layers = []
+                if sum_type!='None':
+                    if sum_type == 'Column':
+                        sum_layer = alt.Chart(groupedTips).mark_bar(size=20, color=sum_color)
+                    elif sum_type == 'Line':
+                        sum_layer = alt.Chart(groupedTips).mark_line(color=sum_color)
+                    elif sum_type == 'Area':
+                        sum_layer = alt.Chart(groupedTips).mark_area(color=sum_color)
 
-        graph.update_layout(
-            plot_bgcolor='rgba(0, 0, 0, 0)',  # Прозрачный фон
-            paper_bgcolor='rgba(0, 0, 0, 0)',
-            font=dict(color='white', size=12),  # Цвет и размер шрифта
-            xaxis=dict(
-                title='Week Start Date',
-                showgrid=True,
-                gridcolor='gray',
-                gridwidth=0.5
-            ),
-            yaxis=dict(
-                title='Sum of Tips',
-                showgrid=True,
-                gridcolor='gray',
-                gridwidth=0.5
-            ),
-            width=1200,
-            height=420,
-            margin=dict(l=40, r=40, t=40, b=40),
-            title=dict(x=0.5),  # Центрирование заголовка
-            coloraxis_showscale=False  # Скрыть цветовую шкалу
-        )
+                    sum_layer = sum_layer.encode(
+                        x=alt.X('WeekStart:T', axis=alt.Axis(title='Week Start Date')),
+                        y=alt.Y('ggTips:Q', axis=alt.Axis(title='Sum of Tips')),
+                        tooltip=['Period', 'ggTips', 'Count']
+                    ).properties(
+                        width=chart_width,
+                        height=chart_height
+                    )
+                    layers.append(sum_layer)
 
+                if count_type!='None':
+                    if count_type == 'Column':
+                        count_layer = alt.Chart(groupedTips).mark_bar(size=20, color=count_color)
+                    elif count_type == 'Line':
+                        count_layer = alt.Chart(groupedTips).mark_line(color=count_color)
+                    elif count_type == 'Area':
+                        count_layer = alt.Chart(groupedTips).mark_area(color=count_color)
 
-        click_event = plotly_events(graph)
+                    count_layer = count_layer.encode(
+                        x=alt.X('WeekStart:T', axis=alt.Axis(title='Week Start Date', titleFontSize=14)),
+                        y=alt.Y('Count:Q', axis=alt.Axis(title='Count of Transactions', titleFontSize=14)),
+                        tooltip=['Period', 'ggTips', 'Count']
+                    ).properties(
+                        width=chart_width,
+                        height=chart_height
+                    )
+                    layers.append(count_layer)
 
-        if click_event:
-            week_num = click_event[0]['x']
-            filtered_data = tips[tips['Week'] == week_num]
-            st.write(f'Transactions for week {week_num}')
-            # selectedColumns = ['Company', 'Partner', 'Date', 'Amount', 'PaymentProcessor', 'ggPayer']
-            st.dataframe(filtered_data)
+                # Комбинирование графиков
+                chart = alt.layer(*layers).resolve_scale(
+                    y='independent'
+                ).configure_axis(
+                    labelColor='white',
+                    titleColor='white'
+                )
+                st.altair_chart(chart, use_container_width=True)
+
+        with tab2:
+            st.write('Companies')
+            st.dataframe(companies)
             
-        st.dataframe(tips)
-        st.dataframe(ggTeammates)
-               
+            st.write('Filtered Data')
+            st.dataframe(filteredTips)
+
     else:
         st.write("import data about ggTips product for analyze")
-    
+
     authenticator.logout()
 
 elif st.session_state['authentication_status'] == False:
     st.error('Username/password is incorrect')
-    
+
 elif st.session_state['authentication_status'] == None:
     st.warning('Please enter your username and password')
-
-
-    
